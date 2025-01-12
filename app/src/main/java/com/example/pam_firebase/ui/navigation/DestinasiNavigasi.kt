a2c132aa36cb4b919994dde9cf84cbcb8f0ae7c6
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-01-12 11:19:25 +0700

    Menambahkan  objek DestinasiHome untuk navigasi

--- app/src/main/java/com/example/pam_firebase/ui/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/pam_firebase/ui/navigation/DestinasiNavigasi.kt
@@ -4,3 +4,7 @@ interface DestinasiNavigasi {
     val route: String
     val titleRes: String
 }
+object DestinasiHome : DestinasiNavigasi {
+    override val route: String = "home"
+    override val titleRes: String = "Home"
+}
