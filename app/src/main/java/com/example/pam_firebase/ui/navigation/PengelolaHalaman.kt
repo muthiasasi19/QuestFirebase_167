d573b37943c071629d7b93bb11b9a9418f6ad655
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-01-12 11:25:51 +0700

    Membuat fungsi pengelola halaman dengan navigasi

---
+++ app/src/main/java/com/example/pam_firebase/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,30 @@
+package com.example.pam_firebase.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pam_firebase.ui.view.HomeScreen
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+
+    }
+}
