8cf61f24a82d7941a1737dacbadb59789b11e2ce
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-01-12 10:27:19 +0700

    Mmebuat fungsi FormMahasiswa untuk input data mahasiswa

--- app/src/main/java/com/example/pam_firebase/ui/view/InsertView.kt
+++ app/src/main/java/com/example/pam_firebase/ui/view/InsertView.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -22,6 +24,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.pam_firebase.ui.viewmodel.PenyediaViewModel
 
@@ -135,5 +138,54 @@ fun InsertBodyMhs(
                 Text("Add")
             }
         }
+    }
+}
+
+
+@Composable
+fun FormMahasiswa(
+    mahasiswaEvent: MahasiswaEvent = MahasiswaEvent(),
+    onValueChange: (MahasiswaEvent) -> Unit,
+    errorState: FormErrorState = FormErrorState(),
+    modifier: Modifier = Modifier
+
+) {
+    val jenisKelamin = listOf("Laki-laki", "Perempuan")
+    val kelas = listOf("A", "B", "C", "D", "E")
+
+    Column (
+        modifier = modifier.fillMaxWidth()
+    ) {
+        mahasiswaEvent.nama?.let {
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = it,
+                onValueChange = {
+                    onValueChange(mahasiswaEvent.copy(nama = it))
+                },
+                label = { Text("Nama") },
+                isError = errorState.nama != null,
+                placeholder = { Text("Masukkan nama") },
+            )
+        }
+        Text(
+            text = errorState.nama ?: "",
+            color = Color.Red
+        )
+        mahasiswaEvent.nim?.let {
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = it, onValueChange = {
+                    onValueChange(mahasiswaEvent.copy(nim = it))
+                },
+                label = { Text("NIM") },
+                isError = errorState.nim != null,
+                placeholder = { Text("Masukkan NIM") },
+                keyboardOptions = KeyboardOptions(keyboardType =
+                KeyboardType.Number)
+            )
+        }
+        Text(text = errorState.nim ?: "", color = Color.Red)
+
     }
 }
