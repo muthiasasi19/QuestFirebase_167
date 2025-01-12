342386f491ac8932170b08feff1532f5d0d38c4c
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-01-12 10:00:57 +0700

    Menampilkan  snackbar dan navigasi saat status form berubah

--- app/src/main/java/com/example/pam_firebase/ui/view/InsertView.kt
+++ app/src/main/java/com/example/pam_firebase/ui/view/InsertView.kt
@@ -1,7 +1,12 @@
 package com.example.pam_firebase.ui.view
 
+
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import com.example.pam_firebase.ui.viewmodel.PenyediaViewModel
 
@@ -17,4 +22,30 @@ fun InsertMhsView(
     val uiEvent = viewModel.uiEvent // State untuk form dan validasi
     val snackbarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
+
+    // Event perubahan state untuk snackbar dan navigasi
+    LaunchedEffect(uiState) {
+        when (uiState) {
+            is FormState.Success -> {
+                println(
+                    "InsertMhsView: uiState is FormState.Success, navigate to home " + uiState.message
+                )
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message) //Tampilkan snackbar
+                }
+                delay(700)
+                // Navigasi langsung
+                onNavigate()
+                viewModel.resetSnackBarMessage() // Reset snackbar state
+            }
+            is FormState.Error -> {
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message)
+                }
+            }
+            else -> Unit
+        }
+    }
+
+
 }
